9011904249b3c2d8a98182c85378ee7de60b257f
spalger <email@spalger.com>
2015-07-25 17:47:19 -0700

    support sourcemaps

--- AutoPreloadRjscommonDepsLoader.js
+++ AutoPreloadRjscommonDepsLoader.js
@@ -2,30 +2,50 @@
 
 'use strict';
 
-let amdRequireRE = /define\(function\s*\(/;
-let requireRE = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g;
-let commentRE = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+var uniq = require('lodash.uniq');
+var loaderUtils = require('loader-utils');
+var SourceNode = require('source-map').SourceNode;
 
-module.exports = function (source) {
+var amdRequireRE = /define\(function\s*\(/;
+var commentRE = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg;
+var requireRE = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g;
+
+var HEADER = '/*** auto-preload-rjscommon-deps-loader ***/\n';
+
+module.exports = function(content, map) {
   this.cacheable();
-  let amd = source.match(amdRequireRE);
-  if (!amd) return source;
 
-  let deps = [];
-  source
+  if (!content.match(amdRequireRE)) {
+    return this.callback(null, content, map);
+  }
+
+  var sourceNode;
+
+  if (map) {
+    sourceNode = SourceNode.fromSourceWithMap(content, map);
+  } else {
+    var fileName = loaderUtils.getRemainingRequest(this);
+    sourceNode = new SourceNode(null, null, null);
+    content.split('\n').forEach(function(line, idx) {
+      sourceNode.add(new SourceNode(idx + 1, 0, fileName, line + '\n'));
+    });
+    sourceNode.setSourceContent(fileName, content);
+  }
+
+  var concatSrc = new SourceNode();
+
+  var deps = [];
+  content
   .replace(commentRE, '')
   .replace(requireRE, function (match, dep) {
-    deps.push(dep);
+    deps.push(`require('${ dep }');`);
   });
 
-  deps = deps.map(function (dep) {
-    return `require('${dep}');`;
-  });
+  concatSrc.add([
+    deps.length ? HEADER + uniq(deps).join('\n') + '\n\n' : '',
+    sourceNode,
+  ]);
 
-  return `${source}
-/* amd deps auto preloaded */
-${deps.join('\n')}
-/* /amd deps auto preloaded */
-`;
-
+  var result = concatSrc.toStringWithSourceMap();
+  this.callback(undefined, result.code, result.map.toString());
 };
